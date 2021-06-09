1a45cee3d4bbf05794506a5ed00627a73c202e64
Maurya232Abhishek <60316864+Maurya232Abhishek@users.noreply.github.com>
2021-06-09 14:37:27 +0530

    Add/Del,Update View.

---
+++ arrays/tempCodeRunnerFile.cpp
@@ -0,0 +1,15 @@
+case 5:
+ {            
+     
+                //int mi=size-1;
+               // if(pos<size-1) mi =pos;
+               cout<<"Array is:_________________________"<<endl;
+                for(int i=0; i<pos && i< size ; i++)
+                {
+                    cout<<a[i]<<" ";
+                }
+                cout<<endl;
+                cout<<"__________________________________"<<endl;
+
+ }
+   
