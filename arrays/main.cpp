a211a0e939f893bdfbc2a909d3236bb80c84d556
champaksworld <champaksworld@users.noreply.github.com>
2021-06-09 11:51:34 +0530

    Add elements to an array

--- arrays/main.cpp
+++ arrays/main.cpp
@@ -4,7 +4,45 @@ using namespace std;
 
 int main()
 {
-    cout << "Arrays in CPP" << endl;
-    cout << "Arrays in CPP by Prashant" << endl;
+    int a[10];
+    int size=sizeof(a)/sizeof(int);
+    int pos=0;
+    while(1)
+    {
+        int option;
+        cout<<"0-exit,1-Add,2-Change,3-Del,4-View"<<endl;
+        cin>>option;
+        switch(option)
+        {
+            case 0:return 0;
+            case 1:
+                {
+                    if(pos>=size)
+                    {
+                        cout<<"Array full"<<endl;
+                        break;
+
+                    }
+                    int value;
+                    cout<<"Enter value";
+                    cin>>value;
+                    a[pos]=value;
+                    pos++;
+                    break;
+                }
+            case 4:
+                {
+                    int i;
+                    cout<<endl;
+                    for(i=0;i<=pos-1;i++)
+                    {
+                        cout<<a[i]<<",";
+                    }
+                    cout<<endl;
+                }
+
+        }
+
+    }
     return 0;
 }
